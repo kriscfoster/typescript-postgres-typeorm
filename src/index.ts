e6aa1816619a9eab0a4bc6189d7fbf4ccc4e9cbf
kriscfoster <krisfoster@live.ie>
2021-11-20 16:19:15 +0000

    fix indentation

--- src/index.ts
+++ src/index.ts
@@ -4,33 +4,23 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import {Request, Response} from "express";
 import {Routes} from "./routes";
-import {User} from "./entity/User";
 
 createConnection().then(async connection => {
+  const app = express();
+  app.use(bodyParser.json());
 
-    // create express app
-    const app = express();
-    app.use(bodyParser.json());
-
-    // register express routes from defined application routes
-    Routes.forEach(route => {
-        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-            const result = (new (route.controller as any))[route.action](req, res, next);
-            if (result instanceof Promise) {
-                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
-
-            } else if (result !== null && result !== undefined) {
-                res.json(result);
-            }
-        });
+  Routes.forEach(route => {
+    (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
+      const result = (new (route.controller as any))[route.action](req, res, next);
+      if (result instanceof Promise) {
+        result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
+      } else if (result !== null && result !== undefined) {
+        res.json(result);
+      }
     });
+  });
 
-    // setup express app here
-    // ...
+  app.listen(3000);
 
-    // start express server
-    app.listen(3000);
-
-    console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results");
-
+  console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results");
 }).catch(error => console.log(error));
